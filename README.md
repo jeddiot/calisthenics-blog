b7f6b6070a86ecb25cc0bbb416e0b4b2b517ea3e
Jedd <nephesh0607@gmail.com>
2025-01-05 23:32:21 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# My Flask App
+
+This is a Flask-based web application deployed on Render.com, demonstrating a calisthenics social media blog.
+
+## Access the Website
+[Visit the website]()
---
+++ calisophy/__init__.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'my_secret_key') # 'mysecret'
+
+#############
+# data base #
+#############
+
+# basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///site.db')# 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+#################
+# login configs #
+#################
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+
+
+from calisophy.core.views import core
+from calisophy.users.views import users
+from calisophy.blog_posts.views import blog_posts
+from calisophy.error_pages.handlers import error_pages
+app.register_blueprint(core)
+app.register_blueprint(users)
+app.register_blueprint(blog_posts)
+app.register_blueprint(error_pages)
